7aa75168a6e7e1c3f49b828bfc3b36b3c8cf70d5
Praneeth Perera <praneethperera52@gmail.com>
2023-12-29 19:17:58 +0530

    Fixed issue

--- app/Http/Controllers/BookingPaymentController.php
+++ app/Http/Controllers/BookingPaymentController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Booking;
 use App\BookingPayment;
-use App\Order;
-use App\OrderPayment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
