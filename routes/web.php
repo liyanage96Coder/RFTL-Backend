f353769af09e7b3c9861709bb48759f8d2057d01
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2025-09-27 19:27:27 +0530

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
 
 Auth::routes();
 
